734288123b06b0207bd8bf5314990154ec565e13
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-09-09 15:29:17 +0530

    Adding state for modal component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,14 +5,14 @@ const Cart = (props) => {
     const cartItems = <ul className="cart-items">{[{id: 'c1', name: 'Shushi', amount: 2, price: 12.99}].map((item) => <li>{item.name}</li>)}</ul>
 
     return(
-        <Modal>
+        <Modal onClose={props.onClose}>
             {cartItems }
             <div className="total">
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className="actions">
-            <button className="button--alts">close</button>
+            <button onClick={props.onClose} className="button--alts">close</button>
             <button className="button">order</button>
             </div>
         </Modal>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM  from "react-dom"
 
 const Backdrop = (props) => {
-    return <div className="backdrop"></div>
+    return <div className="backdrop" onClick={props.onClose}></div>
 }
 
 const ModalOverlay = (props) => {
@@ -16,7 +16,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
     return (
         <React.Fragment>
-            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
         </React.Fragment>
     )
