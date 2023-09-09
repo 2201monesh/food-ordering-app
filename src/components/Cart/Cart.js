fe2bb0335fcd6db64a243ab3f1588c4e14283f36
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-09-09 14:36:39 +0530

    Added Cart Section

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,20 @@
+const Cart = (props) => {
+
+    const cartItems = <ul className="cart-items">{[{id: 'c1', name: 'Shushi', amount: 2, price: 12.99}].map((item) => <li>{item.name}</li>)}</ul>
+
+    return(
+        <div>
+            {cartItems }
+            <div className="total">
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className="actions">
+            <button className="button--alts">close</button>
+            <button className="button">order</button>
+            </div>
+        </div>
+    )
+}
+
+export default Cart;
